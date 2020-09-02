825efe26a269d2efaeddb58839da06d20ffb5959
aishwaryaw <aishwaryawalawalkar@gmail.com>
2020-09-03 01:03:19 +0530

    Update db.js

--- Server/db.js
+++ Server/db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool;
 
 const pool = new Pool({
-    user : "postgres",
-    password : "postgres",
+    user : "username here",
+    password : "password here",
     port : 5432,
     host : "localhost",
-    database : "peanstack"
+    database : "database name here"
 
 });
 
